f67ce3310a1f786d2ee5ad5a7f40a33dd4919614
Thomas Delaney <thomas.delaney@posteo.net>
2018-11-17 01:54:37 +0000

    Initial commit. Includes Bernoulli variable example.

---
+++ README.md
@@ -0,0 +1,15 @@
+A repository for demonstrating some toy examples of Bayesian inference.
+
+Python 2.7.15
+
+Required packages:
+1. argparse 1.1
+2. numpy 1.15.2
+3. matplotlib 2.2.3
+4. datetime
+5. scipy 1.1.0
+
+
+For the default example run:
+
+  python -i py/bernoulli_variable.py
---
+++ py/bernoulli_variable.py
@@ -0,0 +1,75 @@
+"""
+For estimating the mean value of a Bernoulli variable using Maximum a posteriori with a Beta prior.
+Useful lines to include for editing:
+    execfile(os.path.join(os.environ['HOME'], '.pythonrc'))
+"""
+import os
+execfile(os.path.join(os.environ['HOME'], '.pythonrc'))
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime as dt
+from scipy.stats import beta, binom
+
+# command line arguments
+parser = argparse.ArgumentParser(description='Demonstrate Bayesian inference.')
+parser.add_argument('-t', '--true_mu', help='True mean of Bernoulli distn.', type=float, default=0.2)
+parser.add_argument('-p', '--prior_params', help='The parameters of the beta prior', type=float, default=[10,10], nargs=2)
+parser.add_argument('-n', '--num_data_points', help='The number of data points to sample from true distn.', type=int, default=100)
+parser.add_argument('-d', '--debug', help='Enter debug mode.', action='store_true', default=False)
+args = parser.parse_args()
+
+def getTrueDistn(mu):
+    return binom(1, mu)
+
+def getPriorDistn(a,b):
+    return beta(a,b)
+
+def getPosteriorDistn(a,b,x_data):
+    a_n = a + x_data.sum()
+    b_n = b + (x_data.shape[0] - x_data.sum())
+    return beta(a_n, b_n)
+
+def plotPriorDistn(prior_probabilities, possible_mu_values):
+    plt.plot(possible_mu_values, prior_probabilities, color='g')
+    plt.fill_between(possible_mu_values, prior_probabilities, color='green', alpha=0.3, label='prior pdf')
+    plt.xlabel('$\mu$'); plt.ylabel('$p(\mu|\mathbf{x})$')
+    plt.tight_layout()
+
+def plotPosteriorDistn(posterior_probabilities, possible_mu_values, num_data_points):
+    if num_data_points == 1:
+        plt.plot(possible_mu_values, posterior_probabilities, 'r', alpha=0.3, label='posterior pdf')
+    else:
+        plt.plot(possible_mu_values, posterior_probabilities, 'r', alpha=0.3)
+    plt.title('Number of data points used: ' + str(num_data_points))
+
+def main():
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'Starting main function...')
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'Constructing true distribution...')
+    true_distribution = getTrueDistn(args.true_mu)
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'Sampling...')
+    x_data = true_distribution.rvs(size=args.num_data_points)
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'Constructing prior distribution...')
+    prior_distribution = getPriorDistn(args.prior_params[0], args.prior_params[1])
+    possible_mu_values = np.linspace(0, 1, 100)
+    prior_probabilities = prior_distribution.pdf(possible_mu_values)
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'Plotting prior distribution...')
+    fig = plt.figure()
+    plotPriorDistn(prior_probabilities, possible_mu_values)
+    plt.show(block=False)
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'Integrating data and plotting posterior distribution...')
+    for i in range(0, x_data.shape[0]):
+        posterior_distn = getPosteriorDistn(args.prior_params[0], args.prior_params[1], x_data[:i])
+        posterior_probabilities = posterior_distn.pdf(possible_mu_values)
+        plotPosteriorDistn(posterior_probabilities, possible_mu_values, i+1)
+        plt.pause(0.05)
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'Plotting true mu...')
+    plt.vlines(args.true_mu, ymin=0, ymax=plt.ylim()[1], label='true $\mu$', alpha=0.3, linestyles='dashed')
+    plt.legend()
+    plt.show(block=False)
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'Done.')
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'true mu = ' + str(args.true_mu))
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'MAP estimated mu = ' + str(posterior_distn.mean()))
+
+if not(args.debug):
+    main()
