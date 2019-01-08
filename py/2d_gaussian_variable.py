9d05ce662e099bcc679c156d95b73e5f05bce021
Thomas Delaney <thomas.delaney@posteo.net>
2019-01-08 12:16:32 +0000

    Added a 2d gaussian script.

--- py/2d_gaussian_variable.py
+++ py/2d_gaussian_variable.py
@@ -32,16 +32,15 @@ class NormalInverseWishart(object):
         self._nu = nu
         self._invwishart = invwishart(df=nu, scale=psi)
 
-    def pdf(self, x, sigma_sq):
-        sigma = np.sqrt(sigma_sq)
+    def pdf(self, x, sigma):
         invwishart_factor = self._invwishart.pdf(sigma)
-        normal_distn = norm(self._mu, sigma/kappa)
+        normal_distn = multivariate_normal(self._mu, sigma/kappa)
         normal_factor = normal_distn.pdf(x)
         return normal_factor * invwishart_factor
 
     def rvs(self, size=1): # for sampling
         sigmas = self._invwishart.rvs(size=size)
-        mus = np.concatenate([norm(self._mu, s/self._kappa).rvs(size=1) for s in sigmas])
+        mus = np.concatenate([multivariate_normal(self._mu, s/self._kappa).rvs(size=1) for s in sigmas])
         return zip(mus, sigmas)
 
     def mode(self):
@@ -64,24 +63,64 @@ def getAxesLimitsFromDistns(true_distn, prior_modal_distn):
     x_grid, y_grid = np.meshgrid(x_space, y_space)
     return x_grid, y_grid
 
+def processMatrixArgs(flat_true_variance, flat_prior_variance):
+    true_variance = np.array(flat_true_variance).reshape(2,2)
+    prior_variance = np.array(flat_prior_variance).reshape(2,2)
+    return true_variance, prior_variance
+
 def getPriorDistn(prior_mu, prior_kappa, prior_psi, prior_nu):
     return NormalInverseWishart(prior_mu, prior_kappa, prior_psi, prior_nu)
 
-def plotTrueDistn(true_distn, x_grid, y_grid):
+def getPosteriorDistn(prior_mu, prior_kappa, prior_psi, prior_nu, x_data):
+    n = x_data.shape[0]
+    sample_mean = x_data.mean(axis=0)
+    shifted_x_data = x_data - sample_mean
+    sample_covariance = np.array([np.outer(sxd, sxd) for sxd in shifted_x_data]).sum(axis=0)
+    means_covariance = np.outer(sample_mean - prior_mu, sample_mean - prior_mu)
+    post_kappa = prior_kappa + n
+    post_nu = prior_nu + n
+    post_mu = (prior_kappa*prior_mu + n*sample_mean)/post_kappa
+    post_psi = prior_psi + sample_covariance + n*prior_kappa*means_covariance/post_kappa
+    return NormalInverseWishart(post_mu, post_kappa, post_psi, post_nu)
+
+def plot2dGaussianDistn(ax, distn, x_grid, y_grid, title=''):
     plot_space = np.empty(x_grid.shape + (2,))
     plot_space[:,:,0] = x_grid
     plot_space[:,:,1] = y_grid
-    surf = ax.plot_surface(x_grid, y_grid, true_distn.pdf(plot_space), cmap='viridis', linewidth=0)
+    surf = ax.plot_surface(x_grid, y_grid, distn.pdf(plot_space), cmap='viridis', linewidth=0)
+    plt.title(title)
 
-true_distn = multivariate_normal(args.true_mean, np.sqrt(args.true_variance))
-prior_distn_mode = getPriorDistn(np.array(args.prior_mean), args.prior_kappa, np.array(args.prior_variance), args.prior_nu).mode()
-prior_modal_distn = multivariate_normal(prior_distn_mode[0], prior_distn_mode[1])
-x_grid, y_grid = getAxesLimitsFromDistns(true_distn, prior_modal_distn)
-fig = plt.figure()
-ax = fig.add_subplot(121, projection='3d')
-plotTrueDistn(true_distn, x_grid, y_grid)
-plt.show(block=False)
+def main():
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'Starting main function...')
+    args.true_variance, args.prior_variance = processMatrixArgs(args.true_variance, args.prior_variance)
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'Constructing true distribution...')
+    true_distn = multivariate_normal(args.true_mean, args.true_variance)
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'Sampling...')
+    x_data = true_distn.rvs(size=args.num_data_points)
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'Constructing prior distribution...')
+    prior_distn_mode = getPriorDistn(np.array(args.prior_mean), args.prior_kappa, np.array(args.prior_variance), args.prior_nu).mode()
+    prior_modal_distn = multivariate_normal(prior_distn_mode[0], prior_distn_mode[1])
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'Plotting true distribution...')
+    x_grid, y_grid = getAxesLimitsFromDistns(true_distn, prior_modal_distn)
+    fig = plt.figure()
+    ax1 = fig.add_subplot(121, projection='3d')
+    plot2dGaussianDistn(ax1, true_distn, x_grid, y_grid, title='True Distribution')
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'Plotting distribution at prior mode...')
+    ax2 = fig.add_subplot(122, projection='3d')
+    plot2dGaussianDistn(ax2, prior_modal_distn, x_grid, y_grid, title='Prior Distribution')
+    plt.show(block=False)
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'Integrating data and plotting Gaussian pdf at posterior mode...')
+    for i in range(2, x_data.shape[0]): # starting at 2 to avoid a 0 variance
+        posterior_mode = getPosteriorDistn(np.array(args.prior_mean), args.prior_kappa, np.array(args.prior_variance), args.prior_nu, x_data[:i]).mode()
+        posterior_modal_distn = multivariate_normal(posterior_mode[0], posterior_mode[1])
+        ax2.cla()
+        plot2dGaussianDistn(ax2, posterior_modal_distn, x_grid, y_grid, title='Posterior distribution, num points used = '+str(i))
+        plt.pause(0.05)
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'Done.')
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'true mean = ' + str(args.true_mean))
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'MAP estimated mean = ' + str(posterior_modal_distn.mean))
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'true covariance = ' + str(args.true_variance))
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'MAP estimated covariance = ' + str(posterior_modal_distn.cov))
 
-# TODO: Add process args function to make matrices from command line arguments
-#   Add second surface plot for plotting posterior
-#   Add titles
+if not(args.debug):
+    main()
