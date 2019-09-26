36caabc4d3707a03e3063c984b206dcfbfdb2fa5
Thomas James Delaney <thomas.delaney@posteo.net>
2019-09-26 20:38:43 +0100

    Added to the conway-maxwell binomial script.

--- py/conway_maxwell_binomial.py
+++ py/conway_maxwell_binomial.py
@@ -8,6 +8,7 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.special import comb
+from math import log, factorial
 
 parser = argparse.ArgumentParser(description='For defining the Conway-Maxwell binomial distribution in a class, calculating some probabilities and taking some samples.')
 parser.add_argument('-p', '--success_prob', help='Probability of success.', type=float, default=0.5)
@@ -19,7 +20,7 @@ args = parser.parse_args()
 class ConwayMaxwellBinomial(object):
     def __init__(self, p, nu, m):
         """
-        Creates the Conway-Maxwell binomial distribution with parameters p, nu, and m.
+        Creates the Conway-Maxwell binomial distribution with parameters p, nu, and m. Calculates the normalising function during initialisation. Uses exponents and logs to avoid overflow.
         Arguments:  self,
                     p, real 0 <= p <= 1, probability of success
                     nu, real, dispersion parameter
@@ -29,19 +30,19 @@ class ConwayMaxwellBinomial(object):
         self.p = p
         self.nu = nu
         self.m = m
-        self.normaliser = np.sum([(comb(self.m, i)**self.nu) * (self.p**i) * ((1-self.p)**(self.m - i)) for i in range(0, self.m + 1)])
+        self.normaliser = np.sum([np.exp((self.nu * log(comb(self.m, i))) + (i * log(self.p)) + ((self.m - i) * log(1-self.p))) for i in range(0, self.m + 1)])
         self.samp_des_dict = self.getSamplingDesignDict()
 
     def pmf(self, k):
         """
-        Probability mass function.
+        Probability mass function. Uses exponents and logs to avoid overflow.
         Arguments:  self, ConwayMaxwellBinomial object,
                     k, int, must be an integer in the interval [0, m]
         Returns:    P(k)
         """
         if (k > self.m) | (k != int(k)):
             raise ValueError("k must be an integer between 0 and m, inclusive")
-        return ((comb(self.m, k)**self.nu) * (self.p**k) * ((1-self.p)**(self.m-k)))/self.normaliser
+        return np.exp((self.nu * log(comb(self.m, k))) + (k*log(self.p)) + ((self.m-k) * log(1-self.p)))/self.normaliser
     
     def getSamplingDesignDict(self):
         """
@@ -54,6 +55,30 @@ class ConwayMaxwellBinomial(object):
 
     def rvs(self, size=1):
         return np.random.choice(range(0,self.m + 1), size=size, replace=True, p=list(self.samp_des_dict.values()))
+
+class ConwayMaxwellBinomialConjugatePrior(object):
+    def init(self, chi, c, m):
+        """
+        For initialising a conjugate prior for the Conway-Maxwell binomial distribution. 
+        Arguments:  self, object.
+                    chi, 2-d hyperparameters.
+                    c, the count hyperparameter.
+                    m, the number of trials in the Conway-Maxwell distribution
+        Returns:    object
+        """
+        self.chi = chi
+        self.c = c
+        self.m = m
+        self.con_normaliser = np.sum([(comb(self.m, i)**self.chi[1]) * np.exp(i*self.chi[0]) / (1 + np.exp(self.chi[0]))**m  for i in range(0, self.m + 1)])
+
+    def pdf(self, x):
+        """
+        Probability Density fuction. 
+        Arguments:  self, object.
+                    x, 2-d array. 
+        Returns:    P(x)
+        """
+        return np.dot(self.chi, x) - (c * log(factorial(self.m))) + (log(self.con_normaliser))
 
 bernoulli_dist = ConwayMaxwellBinomial(0.5, 1, 1)
 binom_dist = ConwayMaxwellBinomial(0.5, 1, 50)
