b5cbdb94c02243e434f38e086b675833e69a22b2
Thomas James Delaney <thomas.delaney@posteo.net>
2019-10-05 21:24:56 +0100

    posterior function appears to be working now.

--- py/conway_maxwell_binomial.py
+++ py/conway_maxwell_binomial.py
@@ -8,7 +8,7 @@ import argparse
 import warnings
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.special import comb
+from scipy.special import comb, logit
 from math import log, factorial
 
 parser = argparse.ArgumentParser(description='For defining the Conway-Maxwell binomial distribution in a class, calculating some probabilities and taking some samples.')
@@ -75,36 +75,81 @@ class ConwayMaxwellBinomial(object):
         else:
             return np.sum([np.exp((self.nu * log(comb(self.m, i))) + (i * log(self.p)) + ((self.m - i) * log(1-self.p))) for i in range(0, self.m + 1)])
 
+def getLogFactorial(k):
+    """
+    For calculating log(k!) = log(k) + log(k-1) + ... + log(2) + log(1).
+    Arguments:  k, int
+    Returns:    log(k!)
+    """
+    return np.sum([log(i) for i in range(1, k+1)])
+
 def calculateSecondSufficientStat(samples,m):
-  """
-  For calculating the second sufficient stat for the conway maxwell binomial distribution. k!(m-k)!
-  if samples is an array, sums over the samples.
-  Arguments:  samples, integer or array of integers
-              m, the maximum value of any given sample
-  Returns:    \sum_{i=1}^n k_i! (m - k_i)! where k_i is a sample
-  """
-  samples = np.array([samples]) if np.isscalar(samples) else samples
-  return np.sum([log(factorial(sample))  +  log(factorial(m - sample)) for sample in samples])
+    """
+    For calculating the second sufficient stat for the conway maxwell binomial distribution. k!(m-k)!
+    if samples is an array, sums over the samples.
+    Arguments:  samples, integer or array of integers
+                m, the maximum value of any given sample
+    Returns:    \sum_{i=1}^n k_i! (m - k_i)! where k_i is a sample
+    """
+    samples = np.array([samples]) if np.isscalar(samples) else samples
+    return np.sum([getLogFactorial(sample) + getLogFactorial(m - sample) for sample in samples])
+
+def posteriorProprietyTest(a,b,c,m):
+    """
+    The conjugate posterior of the Conway-Maxwell binomial distribution is only proper for certain values of the hyperparameters.
+    This function tests if the hyperparameters are within these values.
+    Arguments:  a, float, the first hyperparameter, corresponds to the first sufficient statistic \sum k_i
+                b, float, the second hyperparameter, corresponds to the seconds sufficient statistic, \sum log(k_i!(m-k_i)!)
+                c, int, the pseudocount hyperparameter.
+    Returns:    None, or raises an error
+    """
+    assert 0 < (a/c), "a/c <= 0"
+    assert (a/c) < m, "a/c >= m"
+    assert -getLogFactorial(m) < (b/c), "-log(m!) >= b/c"
+    floored_ratio = np.floor(a/c).astype(int)
+    ceiled_ratio = np.ceil(a/c).astype(int)
+    larger = -calculateSecondSufficientStat(floored_ratio,m) + ((a/c) - floored_ratio)*(-calculateSecondSufficientStat(ceiled_ratio,m) + calculateSecondSufficientStat(floored_ratio,m))
+    assert (b/c) < larger, "(b/c) > t(floor(a/c)) + (a/c - floor(a/c))(t(ceil(a/c)) - t(floor(a/c)))"
+    return None
 
 def conwayMaxwellBinomialPosteriorKernel(params, prior_params, suff_stats, m, n):
-  """
-  For calculating the kernel of the posterior distribution of a Conway-Maxwell binomial distribution at parameter values 'params'.
-  Parameters are assumed to be in canonical form, rather than natural.
-  Arguments:  params, 2 element 1-d numpy array (float), the parameter values for the Conway-Maxwell binomial distribution
-              prior_params, 2 element list, first element is 2 element 1-d numpy array (float), second element is int, parameters of the prior distribution
-              suff_stats, 2 element array, sufficient statistics of the Conway-Maxwell binomial distribution, calculated from data, (sum(k_i), sum(log(k_i!(m-k_i!))))
-              m, int, the number of bernoulli variables, considered fixed and known.
-              n, int, number of data points
-  Returns: the kernel value at (p, nu) = params
-  """
-  p, nu = params
-  chi, c = prior_params
-  test_dist = ConwayMaxwellBinomial(p, nu, m)
-  natural_params = np.array([np.log(p/(1-p)), -nu])
-  total_count = c + n # includes psuedocounts
-  partition_part = np.log(test_dist.normaliser) - (m*np.log(1-p)) - (nu*np.log(factorial(m)))
-  data_part = np.dot(natural_params, chi + suff_stats)
-  return np.exp(data_part - (total_count*partition_part))
+    """
+    For calculating the kernel of the posterior distribution of a Conway-Maxwell binomial distribution at parameter values 'params'.
+    Parameters are assumed to be in canonical form, rather than natural.
+    Arguments:  params, 2 element 1-d numpy array (float), the parameter values for the Conway-Maxwell binomial distribution
+                prior_params, 2 element list, first element is 2 element 1-d numpy array (float), second element is int, parameters of the prior distribution
+                suff_stats, 2 element array, sufficient statistics of the Conway-Maxwell binomial distribution, calculated from data, (sum(k_i), sum(log(k_i!(m-k_i!))))
+                m, int, the number of bernoulli variables, considered fixed and known.
+                n, int, number of data points
+    Returns: the kernel value at (p, nu) = params
+    """
+    p, nu = params
+    chi, c = prior_params
+    a, b = chi
+    posteriorProprietyTest(a,b,c,m)
+    natural_params = np.array([logit(p), nu])
+    data_part = np.dot(natural_params, chi + suff_stats)
+    total_count = c + n # includes psuedocounts
+    test_dist = ConwayMaxwellBinomial(p, nu, m)
+    partition_part = np.log(test_dist.normaliser) - (nu * getLogFactorial(m)) - (m * np.log(1-p))
+    return np.exp(data_part - total_count*partition_part)
+
+def calcUpperBound(a,c,m):
+    ratio=a/c
+    floored_ratio = np.floor(ratio).astype(int)
+    ceiled_ratio = np.ceil(ratio).astype(int)
+    return -calculateSecondSufficientStat(floored_ratio,m) + (ratio - floored_ratio)*(-calculateSecondSufficientStat(ceiled_ratio,m) + calculateSecondSufficientStat(floored_ratio,m))
+
+def plotConwayMaxwellPmf(com_dist):
+    """
+    For plotting the pmf of the given distribution. 
+    Arguments:  com_dist, distribution
+    Returns:    None
+    """
+    plt.plot(range(0, com_dist.m + 1), list(com_dist.samp_des_dict.values()))
+    plt.xlim(0,com_dist.m)
+    plt.xlabel('k')
+    plt.ylabel('P(k)')
 
 bernoulli_dist = ConwayMaxwellBinomial(0.5, 1, 1)
 binom_dist = ConwayMaxwellBinomial(0.5, 1, 50)
@@ -112,28 +157,18 @@ over_disp_dist = ConwayMaxwellBinomial(0.5, 0.5, 50)
 under_disp_dist = ConwayMaxwellBinomial(0.5, 1.5, 50)
 
 # need to sample from a distribution
-n=100
+n=50
 samples = binom_dist.rvs(size=n)
 # then define the prior parameters
 m=50 # technically a parameter of the distributions, but considered known and fixed
-prior_params = [np.array([1, 0.001]),1]
+prior_params = [np.array([m/2, -120]), 1]
 possible_p_values = np.linspace(0,1, 101)
-possible_nu_values = np.linspace(-1, 1, 201)
+possible_nu_values = np.linspace(-1, 2, 301)
 
-kernel_values = np.array([conwayMaxwellBinomialPosteriorKernel(np.array([p,1]), prior_params, np.array([samples.sum(), calculateSecondSufficientStat(samples, m)]), m, n) for p in possible_p_values])
+kernel_values = np.array([conwayMaxwellBinomialPosteriorKernel(np.array([p,1]), prior_params, np.array([samples.sum(), -calculateSecondSufficientStat(samples, m)]), m, n) for p in possible_p_values])
 plt.subplot(1,2,1)
 plt.plot(possible_p_values, kernel_values)
-kernel_values = np.array([conwayMaxwellBinomialPosteriorKernel(np.array([0.5,nu]), prior_params, np.array([samples.sum(), calculateSecondSufficientStat(samples, m)]), m, n) for nu in possible_nu_values])
+kernel_values = np.array([conwayMaxwellBinomialPosteriorKernel(np.array([0.5,nu]), prior_params, np.array([samples.sum(), -calculateSecondSufficientStat(samples, m)]), m, n) for nu in possible_nu_values])
 plt.subplot(1,2,2)
 plt.plot(possible_nu_values, kernel_values)
-
-#for nu in args.nu_values:
-#    com_bin_dist = ConwayMaxwellBinomial(args.success_prob, nu, args.number_of_trials)
-#    plt.plot(range(0,args.number_of_trials + 1), list(com_bin_dist.samp_des_dict.values()), label='nu = ' + str(nu))
-#plt.legend(fontsize='large')
-#plt.xlabel('k', fontsize='large')
-#plt.ylabel('P(k)', fontsize='large')
-#plt.title('p = ' + str(args.success_prob) + ', num trials = ' + str(args.number_of_trials), fontsize='large')
-#plt.tight_layout()
-#plt.show(block=False)
-
+plt.show(block=False)
